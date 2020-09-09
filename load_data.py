566b99ec41234346e0c3d2f8036a0b5c29bcfc27
axmetES <fraktsia@gmail.com>
2020-09-09 21:13:31 +0600

    - Created files dowloader script.

---
+++ load_data.py
@@ -0,0 +1,28 @@
+import requests
+import pprint
+
+
+def load_address(address_url):
+    response = requests.get(address_url)
+    response.raise_for_status()
+    return response.json()
+
+
+def load_menu(menu_url):
+    response = requests.get(menu_url)
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
+    address_url = 'https://dvmn.org/media/filer_public/90/90/9090ecbf-249f-42c7-8635-a96985268b88/addresses.json'
+    menu_url = 'https://dvmn.org/media/filer_public/a2/5a/a25a7cbd-541c-4caf-9bf9-70dcdf4a592e/menu.json'
+    address = load_address(address_url)
+    menu = load_menu(menu_url)
+
+    # pprint.pprint(address)
+    pprint.pprint(menu)
+
+
+if __name__ == '__main__':
+    main()
